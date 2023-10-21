6f4284c1571451b3e79bc40bacdd8ac21a24330c
Rohith Mulumudy <mulumudyrohith@gmail.com>
2023-10-21 15:20:59 -0400

    fetch tables api added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import evadb
+import lark
+import json
 
 app = Flask(__name__)
 
@@ -15,8 +17,15 @@ def index():
 @app.route('/execute', methods = ['POST'])
 def execute():
     data = request.json['query']
-    res = cursor.query(data).df()
-    return res.to_json(orient ='records')
+    try:
+        res = cursor.query(data).df()
+        return res.to_json(orient ='records')
+    except AssertionError:
+        return {"error": "Please check your syntax"}, 500
+    except lark.exceptions.UnexpectedToken:
+        return {"error": "Unexpected token in your query or incomplete syntax"}, 500
+    except Exception as e:
+        return {"error": "Unexpected error occurred"}, 500
 
 @app.route('/create', methods = ['POST'])
 def create():
@@ -34,6 +43,19 @@ def create():
     """.format(name=data['name'], engine=data['engine'], user=data['user'], password=data['password'], host=data['host'], port=data['port'], db=data['db'])
     res = cursor.query(query).df()
     return res.to_json(orient ='records')
+
+
+@app.route('/tables', methods = ['GET'])
+def get_tables():
+    res = cursor.query("SHOW tables").df()
+    lst = res.values.tolist()
+    tables = [table for [table] in lst]
+
+    return json.dumps(tables)
+
+def run():
+    return app
+
 
 
 if __name__ == "__main__":
